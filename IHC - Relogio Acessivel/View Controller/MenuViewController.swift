270a01a9182aef103e9a849c18597ba08aa1ac98
Gui Reis <gui.sreis25@gmail.com>
2021-11-08 04:35:02 -0300

    Initial commit

---
+++ IHC - Relogio Acessivel/View Controller/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IHC - Relogio Acessivel
+//
+//  Created by Gui Reis on 08/11/21.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# IHC-Relogio-Acessivel
+ Projeto da faculdade da matéria de Interação Humano Computador do 6º Semestre
